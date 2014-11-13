7b6ff2fb0750e0f4c20a86d8616b2966fa5d6824
Doug Royal <email@dougroyal.com>
2014-11-12 21:38:45 -0600

    started working on enabling custom clients

--- scm_cli/client_loader.py
+++ scm_cli/client_loader.py
@@ -8,15 +8,18 @@ if sys.version_info[0] >= 3:
 else:
     from ConfigParser import ConfigParser
 
-# from os.path import expanduser
-# USER_HOME = expanduser("~")
-# SCM_RC_DIR = os.path.join(USER_HOME, '.scm')
+from os.path import expanduser
+USER_HOME = expanduser("~")
+SCM_RC_DIR = os.path.join(USER_HOME, '.scm')
+CUSTOM_CLIENTS_DIR = os.path.join(SCM_RC_DIR, 'clients')
 
+# enable ~/.scm/clients
+sys.path.append(CUSTOM_CLIENTS_DIR)
 
 def load_client_modules():
     clients = {}
 
-    # TODO also search ~/.scm/host_clients/ for custom clients
+    import pprint; pprint.pprint(sys.path)
     for host in _get_host_clients():
         module = "scm_cli.clients.%s_client" % host
         clients[host] = __import__(module, globals(), locals(), ['object'], 0)
--- scm_cli/shell.py
+++ scm_cli/shell.py
@@ -1,5 +1,14 @@
 import subprocess
 
 def run(command):
-    p = subprocess.Popen(command.split())
-    p.wait()
+    p = subprocess.Popen(command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+
+    output, error = p.communicate()
+
+    if error:
+        print(error)
+        raise(Excepiton, error)
+
+    return output
